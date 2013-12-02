c9ed90f2179a7447162320972c89ad70311e448a
akallin <ann.kallin@gmail.com>
2013-12-02 13:52:02 -0500

    replaced gigantic vector multiplication with Eigen.h matrix operations. It's faster than LAPACK SVD too.  Check out the wiki page on the Renyi Measurement Speed-up. For order 15 it used to take 14 days and now it takes 3.5 hours

--- code/entropy.h
+++ code/entropy.h
@@ -2,14 +2,22 @@
 #ifndef entropy_H
 #define entropy_H
 #include<time.h>
+#include<Eigen/Dense>
 
-void getEE( vector <double>& alpha1, vector <double>& CornLineEnts, vector< vector<double> >& SuperMat );
+
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+//void getEE( vector <double>& alpha1, vector <double>& CornLineEnts, vector< vector<double> >& SuperMat );
+void getEE( vector <double>& alpha1, vector <double>& CornLineEnts, Eigen::MatrixXd&  SuperMat );
 long unsigned int regionDim_NA_N( unsigned int na, unsigned int n, vector<unsigned long int> &Abasis, vector<unsigned long int> &AbasPos );
 
 const std::string currentDateTime();
 
 inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< pair<double,double> >& ents, 
         vector< vector< int > >& RScoords, vector <long unsigned int> Basis){
+
 
 
     // Get the graph dimensions from the realspace coordinates
@@ -26,7 +34,8 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
     vector <long unsigned int> Abasis, Bbasis, AbasPos, BbasPos;
 
     // A rectangular matrix containing the eigenvalues, used to get the RDM
-    vector< vector<double > > SuperMat;
+    //vector< vector<double > > SuperMat;
+    MatrixXd DuperMat;
 
     // Some temp variables;
     long unsigned int tempState(-1);         // The current full basis state we're looking at
@@ -66,9 +75,16 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
 
         // Initialize the matrix of eigenvalues
         cout << currentDateTime() << " Initialize Supermat" << endl;
-        SuperMat.clear();
-        SuperMat.resize(Adim);
-        for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
+        //SuperMat.clear();
+        //SuperMat.resize(Adim);
+
+        DuperMat.resize(Adim,Bdim);
+        for(int ad=0;ad<Adim;ad++){
+            for(int bd=0;bd<Bdim;bd++){
+                DuperMat(ad,bd)=0;
+            }
+        }
+        //for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
         cout << currentDateTime() << " .... Supermat created" << endl;
 
         // Loop over all the basis states
@@ -149,18 +165,20 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
             // Unshift bState by 1 (because there was one extra)
             bState = bState>>1;
 
-            SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+            //SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+            DuperMat(AbasPos[aState],BbasPos[bState]) = eigs[i];
         }
         cout << currentDateTime() <<"   ... Supermat filled" << endl;
 
         // ------ GET ENTROPY!!! ------
-        getEE(alpha1, tempEnt, SuperMat);
+        getEE(alpha1, tempEnt, DuperMat);
         for(int a=0; a<alpha1.size(); a++){
             ents[a].first += tempEnt[a];
             ents[a].second += -(xMax-1)*tempEnt[a];
             if(ySize<(yMax+1)/2){ents[a].first += tempEnt[a];  ents[a].second += -(xMax-1)*tempEnt[a];}
         }
     }
+
 
 
     // -*-*-*-*-*-*-*- Vertical -*-*-*-*-*-*-*-*-
@@ -177,18 +195,24 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
         // Iterate over the vectical cuts
         for(int xSize=1; xSize<=xMax/2; xSize++){
 
-            cout << currentDateTime() << " Line term V" << xSize << endl;
+            cout << currentDateTime() << " Line term V" << xSize ;
 
             // Get the dimensions of region A and B;
             Adim = regionDim_NA_N(xSize*ySize, Nsite, Abasis, AbasPos);
             Bdim = regionDim_NA_N(Nsite - xSize*ySize, Nsite, Bbasis, BbasPos);
-            //cout << "Adim = " << Adim << "  Bdim = " << Bdim << endl;
+            cout << "   Adim = " << Adim << "  Bdim = " << Bdim << endl;
 
             // Initialize the matrix of eigenvalues
             cout << currentDateTime() << " Initialize Supermat" << endl;
-            SuperMat.clear();
-            SuperMat.resize(Adim);
-            for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
+            //SuperMat.clear();
+            //SuperMat.resize(Adim);
+            //for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
+            DuperMat.resize(Adim,Bdim);
+            for(int ad=0;ad<Adim;ad++){
+                for(int bd=0;bd<Bdim;bd++){
+                    DuperMat(ad,bd)=0;
+                }
+            }
             cout << currentDateTime() << " .... Supermat created" << endl;
 
             // Loop over all the basis states
@@ -268,12 +292,14 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
                 bState = bState>>1;
 
                 if(AbasPos[aState]<0 || BbasPos[bState]<0){ cout << "SUPER ERROR!" << endl; exit(1);}
-                SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+                //SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+                DuperMat(AbasPos[aState],BbasPos[bState]) = eigs[i];
+
             }
             cout << currentDateTime() <<"   ... Supermat filled" << endl;
 
             // ------ GET ENTROPY!!! ------
-            getEE(alpha1, tempEnt, SuperMat);
+            getEE(alpha1, tempEnt, DuperMat);
             for(int a=0; a<alpha1.size(); a++){
 
                 ents[a].first += tempEnt[a];
@@ -289,17 +315,24 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
         for(int xSize=1; xSize<xMax; xSize++){
            
             if(xMax==yMax){if(xSize<ySize){continue;}} 
-            cout << currentDateTime() << " Corner term " << ySize*xSize << endl;
+            cout << currentDateTime() << " Corner term " << ySize*xSize ;
 
             // Get the dimensions of region A and B;
             Adim = regionDim_NA_N(xSize*ySize, Nsite, Abasis, AbasPos);
             Bdim = regionDim_NA_N(Nsite - xSize*ySize,Nsite, Bbasis, BbasPos);
+            cout << "   Adim = " << Adim << "  Bdim = " << Bdim << endl;
 
             // Initialize the matrix of eigenvalues
             cout << currentDateTime() << " Initialize Supermat" << endl;
-            SuperMat.clear();
-            SuperMat.resize(Adim);
-            for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
+            //SuperMat.clear();
+            //SuperMat.resize(Adim);
+            //for(long unsigned int q=0; q<Adim; q++){ SuperMat[q].resize(Bdim); }
+            DuperMat.resize(Adim,Bdim);
+            for(int ad=0;ad<Adim;ad++){
+                for(int bd=0;bd<Bdim;bd++){
+                    DuperMat(ad,bd)=0;
+                }
+            }
             cout << currentDateTime() << " .... Supermat created" << endl;
 
             // Loop over all the basis states
@@ -378,18 +411,21 @@ inline void Entropy2D(vector <double>& alpha1, vector<l_double>& eigs, vector< p
                 bState = bState>>1;
 
                 if(AbasPos[aState]<0 || BbasPos[bState]<0){ cout << "SUPER ERROR! " << endl; exit(1);}
-                SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+                //SuperMat[AbasPos[aState]][BbasPos[bState]] = eigs[i];
+                DuperMat(AbasPos[aState],BbasPos[bState]) = eigs[i];
+
             }
             
             cout << currentDateTime() <<"   ... Supermat filled" << endl;
             // ------ GET ENTROPY!!! ------
-            getEE(alpha1, tempEnt, SuperMat);
+            getEE(alpha1, tempEnt, DuperMat);
             for(int a=0; a<alpha1.size(); a++){
                 ents[a].second += 2.*tempEnt[a];
                 if(xMax==yMax && xSize!=ySize){ents[a].second += 2.*tempEnt[a];}
             }//loop over alphas
         }//loop over xSize
     }//loop over ySize
+    
 }
 //End of Entropy2D
 
@@ -448,14 +484,25 @@ long unsigned int regionDim_NA_N( unsigned na1, unsigned n1, vector<long unsigne
 /* -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
    getEE
    -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- */
-void getEE(vector <double> & alpha2, vector<double > & CornLineEnts, vector< vector<double> >& SuperMat ){
-    // The Density Matrix
-    double *DM; //create a pointer to a c-style array
+void getEE(vector <double> & alpha2, vector<double > & CornLineEnts, Eigen::MatrixXd& SuperMat ){
+    
+      cout << currentDateTime() << " Multiplying to get DM" << endl;
+      int DMdim;
+      MatrixXd DM;
+    if(SuperMat.cols() >= SuperMat.rows()){
+        DMdim = SuperMat.rows();
+        DM.resize(DMdim,DMdim);
+        DM = SuperMat * SuperMat.transpose();
+    }
+    else{
+        DMdim = SuperMat.cols();
+        DM.resize(DMdim,DMdim);
+        DM = SuperMat.transpose() * SuperMat;
+    }
 
-    double temp(0);
-    long int Dim(0);
-    long int Dim_sq;
-
+      cout << currentDateTime() << "Beginning Diagonalization" << endl;
+      VectorXd dd = DM.selfadjointView<1>().eigenvalues();
+/*
     cout << currentDateTime() << " Getting EE" << endl;
     // Using SuperMat to get the density matrix
     // If Adim > Bdim TRANSPOSE!!
@@ -516,19 +563,27 @@ void getEE(vector <double> & alpha2, vector<double > & CornLineEnts, vector< vec
 
     //clean up DM, unless you need it anywhere below
     delete [] DM;
-
+*/
     double EE(0);
-    double vN(0), renyi(0); 
-    temp=0;
+    double renyi(0); 
+    VectorXd logdd = dd.array().abs().log().matrix(); 
+    VectorXd renyipow;
+    double vN;     
+    int temp=0;
 
+    vN = - (dd.dot(logdd));
+    
     cout << currentDateTime() << " calculating renyis " << endl;
     for(int a=0; a<alpha2.size(); a++){
         EE=0;
-        vN=0;
         renyi=0;
         temp=0;
 
+
+        renyipow = dd.array().pow(alpha2[a]).matrix();
+        renyi = renyipow.sum();
         // Loop over the eigenvalues
+        /*
         for(int s=0; s<dd.size(); s++){
 
             if(dd[s]<0){dd[s]=0;} // All eigs should be positive.  If not it's rounding error.
@@ -536,13 +591,12 @@ void getEE(vector <double> & alpha2, vector<double > & CornLineEnts, vector< vec
             temp=log(dd[s]); 
             if(!(temp>-1000000000)){temp=0;}
 
-            vN += -dd[s]*temp;
-
+         
             // Same problem. If they're too small they get set to 0.
             if(fabs(dd[s])<1e-15){dd[s]=0;}
 
             renyi+=pow(dd[s],alpha2[a]);
-        }    
+        }    */
 
         if(fabs(alpha2[a]-1.0)<0.000001){EE = vN;}
         else{EE = 1./(1.-alpha2[a])*log(renyi);}
